bb0d7851e334c5cf89fb7c8c64ec0d6ce95263c1
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-10-26 13:38:08 -0700

    length of last word

--- app.js
+++ app.js
@@ -19,3 +19,22 @@ var mySqrt = function(x) {
     }
     return root - 1;
 };
+
+////////////////////////
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+var lengthOfLastWord = function(s) {
+    let stringArr = s.trim().split(' ');
+    return stringArr[stringArr.length-1].length;
+};
