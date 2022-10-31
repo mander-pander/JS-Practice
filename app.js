9fafc1c6f9236cb46804f495bc0d0933aad86d4c
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-10-31 14:54:45 -0700

    palindrome

--- app.js
+++ app.js
@@ -140,3 +140,26 @@ var deleteDuplicates = function(head) {
     }
     return temp.next;
 };
+
+/////////////////////////////
+
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+var isPalindrome = function(x) {
+    if (x < 0 ) return false;
+    //change to string and then reverse
+    const rev = `${x}`.split('').reverse().join('');
+    return x == rev;
+};
