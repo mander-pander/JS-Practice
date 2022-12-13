576689b6e41e5c550afac4135c9fd6bb24082b2a
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-12-13 14:22:45 -0800

    majority element

--- app.js
+++ app.js
@@ -293,3 +293,28 @@ var generate = function(numRows) {
     }
     return pascal;
 };
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+//You may assume that the majority element always exists in the array.
+
+//  
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+
+var majorityElement = function(nums) {
+    //sort number array
+    nums.sort((a, b) => a - b);
+    //divide array length by 2, round down in case of odd length
+    return nums[Math.floor((nums.length) / 2)]
+};
