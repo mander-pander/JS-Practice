0501576a3f6e1aa975f6daaeef329c0e72e32efb
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-12-14 10:43:42 -0800

    single number with XOR and reduce

--- app.js
+++ app.js
@@ -318,3 +318,42 @@ var majorityElement = function(nums) {
     //divide array length by 2, round down in case of odd length
     return nums[Math.floor((nums.length) / 2)]
 };
+
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+//  
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
+
+
+var singleNumber = function(nums) {
+// XOR is a binary operation, it stands for "exclusive or", that is to say the      resulting bit evaluates to one if only exactly one of the bits is set.
+
+//This is its function table:
+
+// a | b | a ^ b
+// --|---|------
+// 0 | 0 | 0
+// 0 | 1 | 1
+// 1 | 0 | 1
+// 1 | 1 | 0
+
+//array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+
+    return nums.reduce((prev, curr) => (prev ^ curr), 0);
+};
