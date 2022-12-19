76150aa1be0d024f65633a64749c4aa8ce147e15
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-12-19 11:43:05 -0800

    add digits

--- app.js
+++ app.js
@@ -444,3 +444,33 @@ var isPowerOfTwo = function(n) {
 };
 
 //2 ** x
+
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+//  
+
+// Example 1:
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2
+// Since 2 has only one digit, return it.
+
+// Example 2:
+
+// Input: num = 0
+// Output: 0
+
+
+var addDigits = function(num) {
+    if (num < 10) return num;
+    let numbers = String(num).split('');
+    let sum = 0;
+    for (const number of numbers) {
+        sum += parseInt(number);
+    }
+
+    return addDigits(sum);
+};
