afc92b15154d190db65a7ca0e86b08f5265cc98a
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-12-19 08:42:16 -0800

    power of two

--- app.js
+++ app.js
@@ -405,3 +405,42 @@ var summaryRanges = function(nums) {
     }
     return range;
 };
+
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+//  
+
+// Example 1:
+
+// Input: n = 1
+// Output: true
+// Explanation: 20 = 1
+
+// Example 2:
+
+// Input: n = 16
+// Output: true
+// Explanation: 24 = 16
+
+// Example 3:
+
+// Input: n = 3
+// Output: false
+
+
+var isPowerOfTwo = function(n) {
+    if (n === 0) {
+        return false;
+    }
+    while (n !== 1) {
+        if (n%2 !== 0) {
+            return false;
+        }
+        n = n/2;
+    }
+    return true;
+};
+
+//2 ** x
