50cb14ea83ae7204b758b273a042852b8dc3b2c3
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-12-20 08:28:47 -0800

    ugly number

--- app.js
+++ app.js
@@ -474,3 +474,46 @@ var addDigits = function(num) {
 
     return addDigits(sum);
 };
+
+// An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+// Given an integer n, return true if n is an ugly number.
+
+//  
+
+// Example 1:
+
+// Input: n = 6
+// Output: true
+// Explanation: 6 = 2 × 3
+
+// Example 2:
+
+// Input: n = 1
+// Output: true
+// Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+
+// Example 3:
+
+// Input: n = 14
+// Output: false
+// Explanation: 14 is not ugly since it includes the prime factor 7.
+
+
+var isUgly = function(n) {
+    //while n is greater than 1
+    while (n > 1) {
+        //check to see if n is divisible by 2, 3, or 5
+        //if it is, divide by the number
+        if (n % 2 == 0) {
+            n /= 2;
+        } else if (n % 3 == 0) {
+            n /= 3;
+        } else if (n % 5 == 0) {
+            n /= 5;
+        }
+        else return false;
+    }
+    //return true if number was divided by 2, 3, or 5 until it reaches to 1
+    return n === 1;
+};
