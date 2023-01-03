cbd62eb515d249ea1fb9cff62a5fa4565b7fbbd0
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-01-03 09:21:34 -0800

    palindrome check without built in methods

--- 1-3.js
+++ 1-3.js
@@ -1,3 +1,16 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+//  
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
 var isPalindrome = function(s) {
     //remove non-alphanumeric chars
     let regexPattern = /[^A-Za-z0-9]/g;
@@ -8,4 +21,15 @@ var isPalindrome = function(s) {
     let reversePhrase =   phrase.split('').reverse().join('');
     //compare phrase and reversed phrase
     return phrase === reversePhrase;
+};
+
+//without built in methods
+ const isPalindrome = s => {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+
+    for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+        if (s.charAt(i) !== s.charAt(j)) return false;
+    }
+    
+    return true;
 };
