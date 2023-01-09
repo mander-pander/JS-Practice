d0fc60a244f8dbcfbdae0840c5fefc5e8065bcd5
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-01-09 10:45:34 -0800

    find pivot index

--- January/1-9.js
+++ January/1-9.js
@@ -55,3 +55,64 @@ var runningSum = function(nums) {
     }
     return sum;
 };
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+//  
+
+// Example 1:
+
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+// Example 2:
+
+// Input: nums = [1,2,3]
+// Output: -1
+// Explanation:
+// There is no index that satisfies the conditions in the problem statement.
+
+// Example 3:
+
+// Input: nums = [2,1,-1]
+// Output: 0
+// Explanation:
+// The pivot index is 0.
+// Left sum = 0 (no elements to the left of index 0)
+// Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+var pivotIndex = function(nums) {
+    let rightSum = 0;
+    let leftSum = 0;
+
+	// Calculate the sum of the entire array
+    //stored in rightSum
+    nums.forEach((num) => (rightSum += num));
+
+    for (let i = 0; i < nums.length; i++) {
+        let currNum = nums[i];
+		//subtracting the number at current index from right sum which holds total of array from our first step
+        rightSum -= currNum;
+        //if the leftSum and rightSum are equal, return current index
+        //otherwise add current index to leftside sum after it was initialized at 0 above
+        //process will continue until the sides equal or -1 will be returned
+
+        if (leftSum === rightSum) {
+            return i;
+        } else {
+            leftSum += currNum;
+        }
+    }
+
+    return -1;
+};
