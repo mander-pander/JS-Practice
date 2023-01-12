da023cb912bf88ca1c5d7a93b49ca39900053043
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-01-12 08:20:09 -0800

    linked list cycle, note: need to work on understanding of linked list traversing

--- January/1-12.js
+++ January/1-12.js
@@ -26,3 +26,59 @@ var middleNode = function(head) {
 
     return point1;
 };
+
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+//following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is
+//connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+// Do not modify the linked list.
+
+//  
+
+// Example 1:
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// Example 2:
+
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+// Example 3:
+
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+
+var detectCycle = function(head) {
+    let slow = head
+    let fast = head;
+    // looping while fast and fast.next are not = to null
+    while (fast !== null && fast.next !== null) {
+        //moving the slow pointer one at a time
+        //moving the fast pointer two at a time
+        slow = slow.next;
+        fast = fast.next.next;
+        //once the cycle is found, break
+        if (slow == fast) {
+            break;
+        }
+    }
+    //if there isn't a cycle, return null
+    if (fast == null || fast.next == null) {
+        return null;
+    }
+    // run loop until head & slow don't collab
+    //move the head and slow by one
+    while (head !== slow) {
+        head = head.next;
+        slow = slow.next;
+    }
+
+    return head;
+};
